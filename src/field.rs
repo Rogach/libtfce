621785ab9e34b1550b054bdcc006a03ff0da84d0
Rogach <platon7pronko@gmail.com>
2017-09-02 13:19:01 +0300

    implement 2d4c fields

--- src/field.rs
+++ src/field.rs
@@ -19,3 +19,29 @@ pub fn generate_1d_field(size: usize, min_value: f64, max_value: f64) -> Vec<Vox
     }
     voxels
 }
+
+pub fn generate_2d4c_field(width: usize, min_value: f64, max_value: f64) -> Vec<Voxel> {
+    assert!(width >= 2);
+    let mut rng = StdRng::from_seed(&[17556, 31771, 29830, 29830]);
+
+    let mut voxels = Vec::new();
+    for x in 0..width {
+        for y in 0..width {
+            let mut links = Vec::new();
+            if x < width - 1 {
+                links.push(y * width + x + 1);
+            }
+            if x > 0 {
+                links.push(y * width + x - 1);
+            }
+            if y < width - 1 {
+                links.push((y + 1) * width + x);
+            }
+            if y > 0 {
+                links.push((y - 1) * width + x);
+            }
+            voxels.push(Voxel::new(rng.gen_range(min_value, max_value), links));
+        }
+    }
+    voxels
+}
--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,56 @@ mod field;
 mod tfce;
 mod ttest;
 
-use ::field::generate_1d_field;
+use ::field::generate_2d4c_field;
 use ::tfce::tfce;
 use ::tfce::approximate_tfce::approximate_tfce;
 
 fn main() {
-    let voxels = generate_1d_field(100, 0.0, 10.0);
+    let n = 2;
+    let voxels = generate_2d4c_field(n, 0.0, 1.0);
 
+    println!("import numpy");
     println!("from matplotlib import pyplot");
-    {
-        let mut voxels = voxels.clone();
-        approximate_tfce(&mut voxels, 50);
-        println!("pyplot.plot({:?}, 'r')", voxels.iter().map(|v| v.tfce_value).collect::<Vec<f64>>());
-    }
-    {
-        let mut voxels = voxels.clone();
-        tfce(&mut voxels);
-        println!("pyplot.plot({:?}, 'b')", voxels.iter().map(|v| v.tfce_value).collect::<Vec<f64>>());
-    }
+
+    let orig_data = voxels.iter().map(|v| v.value).collect::<Vec<f64>>();
+    println!("pyplot.subplot2grid((3, 2), (0,0))");
+    println!("pyplot.title('orig data')");
+    println!(
+        "pyplot.imshow(numpy.array({:?}).reshape({},{}), cmap='Greys')",
+        orig_data, n, n
+    );
+
+    let mut approx_voxels = voxels.clone();
+    approximate_tfce(&mut approx_voxels, 100);
+    let approx_data = approx_voxels.iter().map(|v| v.tfce_value).collect::<Vec<f64>>();
+    println!("pyplot.subplot2grid((3,2), (0, 1))");
+    println!("pyplot.title('approximate')");
+    println!(
+        "pyplot.imshow(numpy.array({:?}).reshape({},{}), cmap='Greys')",
+        approx_data, n, n
+    );
+
+    let mut exact_voxels = voxels.clone();
+    tfce(&mut exact_voxels);
+    let exact_data = exact_voxels.iter().map(|v| v.tfce_value).collect::<Vec<f64>>();
+    println!("pyplot.subplot2grid((3,2), (1,0))");
+    println!("pyplot.title('exact')");
+    println!(
+        "pyplot.imshow(numpy.array({:?}).reshape({},{}), cmap='Greys')",
+        exact_data, n, n
+    );
+
+    println!("pyplot.subplot2grid((3,2), (1,1))");
+    println!("pyplot.title('diff')");
+    println!(
+        "pyplot.imshow((numpy.array({:?})-numpy.array({:?})).reshape({},{}), cmap='coolwarm', vmin=-0.1, vmax=0.1)",
+        exact_data, approx_data, n, n
+    );
+
+    println!("pyplot.subplot2grid((3,2), (2,0), 1, 2)");
+    println!("pyplot.title('raw')");
+    println!("pyplot.plot({:?}, 'r')", approx_data);
+    println!("pyplot.plot({:?}, 'b')", exact_data);
+
     println!("pyplot.show()");
 }
